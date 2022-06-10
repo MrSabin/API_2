77e2833014a069f5d349bb813affcf704cba1a2d
MrSabin <sabin.21011986@gmail.com>
2022-06-10 17:08:20 +0300

    Finished step 7

--- step3.py
+++ step3.py
@@ -17,9 +17,22 @@ def shorten_link(token, url):
 	answer = response.json()
 	return answer.get("id")
 
+def count_clicks(token, link):
+	bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary"
+
+	headers = {"Authorization" : f"Bearer {token}"}
+	params = {"units" : "-1"}
+
+	response = requests.get(bitly_url, headers=headers, params=params)
+	response.raise_for_status()
+	answer = response.json()
+	return answer.get("total_clicks")
+
 token = "5980aab052614ad9cb069a667062873e38f81eda"
 
 try:
-	print('Битлинк', shorten_link(token, args.url))
+	bitlink = shorten_link(token, args.url)
+	print('Битлинк', bitlink)
+	print('Количество кликов:', count_clicks(token, bitlink))
 except requests.exceptions.HTTPError:
 	print("Задана неверная ссылка")
