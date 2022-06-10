0e5b44962ce5bb1322df0410710d8d030f21323d
MrSabin <sabin.21011986@gmail.com>
2022-06-10 20:18:36 +0300

    Some changes
    - deleted step2.py (not needed anymore)
    - renamed step3.py

--- step2.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://api-ssl.bitly.com/v4/user'
-headers = {
-	"Authorization" : "Bearer 5980aab052614ad9cb069a667062873e38f81eda"
-}
-response = requests.get(url, headers=headers)
-print(response.text)
