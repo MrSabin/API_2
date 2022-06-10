c02504b8daf18856beb198787fe12888baf3d76e
MrSabin <sabin.21011986@gmail.com>
2022-06-10 15:57:33 +0300

    Finished step 5

--- step3.py
+++ step3.py
@@ -1,16 +1,16 @@
+import argparse
+
 import requests
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-url', help = 'URL для сокращения')
+args = parser.parse_args()
 
 def shorten_link(token, url):
 	bitly_url = "https://api-ssl.bitly.com/v4/bitlinks"
 
-	headers = {
-		"Authorization" : f"Bearer {token}"
-	}
-
-	body = {
-		"long_url" : f"{url}"
-	}
+	headers = {"Authorization" : f"Bearer {token}"}
+	body = {"long_url" : f"{url}"}
 
 	response = requests.post(bitly_url, headers=headers, json=body)
 	answer = response.json()
@@ -19,4 +19,4 @@ def shorten_link(token, url):
 token = "5980aab052614ad9cb069a667062873e38f81eda"
 url = "https://osu.ppy.sh/users/12782105"
 
-print('Битлинк', shorten_link(token, url))
+print('Битлинк', shorten_link(token, args.url))
