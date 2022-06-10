48390e2729c8f0b4c22ac963de133b8674e65b16
MrSabin <sabin.21011986@gmail.com>
2022-06-10 15:21:42 +0300

    Finished step 4

---
+++ step3.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def shorten_link(token, url):
+	bitly_url = "https://api-ssl.bitly.com/v4/bitlinks"
+
+	headers = {
+		"Authorization" : f"Bearer {token}"
+	}
+
+	body = {
+		"long_url" : f"{url}"
+	}
+
+	response = requests.post(bitly_url, headers=headers, json=body)
+	answer = response.json()
+	return(answer.get("id"))
+
+token = "5980aab052614ad9cb069a667062873e38f81eda"
+url = "https://osu.ppy.sh/users/12782105"
+
+print('Битлинк', shorten_link(token, url))
