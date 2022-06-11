ce997a2a03918d611963160dab5a0bf4cfe027cc
MrSabin <sabin.21011986@gmail.com>
2022-06-11 17:32:11 +0300

    Formatted code according to PEP8

--- main.py
+++ main.py
@@ -9,44 +9,46 @@ load_dotenv()
 token = os.environ["TOKEN"]
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-url', help = 'URL для сокращения')
+parser.add_argument('-url', help='URL для сокращения')
 args = parser.parse_args()
 
 
 def is_bitlink(url):
-	bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}"
-	headers = {"Authorization" : f"Bearer {token}"}
-	response = requests.get(bitly_url, headers=headers)
-	return response.ok
+    bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(bitly_url, headers=headers)
+    return response.ok
 
 
 def shorten_link(token, url):
-	bitly_url = "https://api-ssl.bitly.com/v4/bitlinks"
-	headers = {"Authorization" : f"Bearer {token}"}
-	body = {"long_url" : f"{url}"}
-	response = requests.post(bitly_url, headers=headers, json=body)
-	response.raise_for_status()
-	answer = response.json()
-	return answer.get("id")
+    bitly_url = "https://api-ssl.bitly.com/v4/bitlinks"
+    headers = {"Authorization": f"Bearer {token}"}
+    body = {"long_url": f"{url}"}
+    response = requests.post(bitly_url, headers=headers, json=body)
+    response.raise_for_status()
+    answer = response.json()
+    return answer.get("id")
 
 
 def count_clicks(token, url):
-	bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary"
-	headers = {"Authorization" : f"Bearer {token}"}
-	params = {"units" : "-1"}
-	response = requests.get(bitly_url, headers=headers, params=params)
-	response.raise_for_status()
-	answer = response.json()
-	return answer.get("total_clicks")
+    bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary"
+    headers = {"Authorization": f"Bearer {token}"}
+    params = {"units": "-1"}
+    response = requests.get(bitly_url, headers=headers, params=params)
+    response.raise_for_status()
+    answer = response.json()
+    return answer.get("total_clicks")
+
 
 def main():
-	try:
-		if is_bitlink(args.url):
-			print('Количество кликов:', count_clicks(token, args.url))
-		else:
-			print('Битлинк', shorten_link(token, args.url))
-	except requests.exceptions.HTTPError:
-		print("Задана неверная ссылка")
+    try:
+        if is_bitlink(args.url):
+            print('Количество кликов:', count_clicks(token, args.url))
+        else:
+            print('Битлинк', shorten_link(token, args.url))
+    except requests.exceptions.HTTPError:
+        print("Задана неверная ссылка")
+
 
 if __name__ == '__main__':
     main()
